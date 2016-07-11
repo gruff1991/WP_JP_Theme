354cc71ac26412bcbc9b548fd9593710feed0b07
Grant Spooner-Roberts <grantroberts@live.co.uk>
2016-07-11 16:47:46 +0100

    update functions.php (#1)
    created a setup theme to put all the hooks in so only one function needed to be called

--- functions.php
+++ functions.php
@@ -1,4 +1,66 @@
 <?php
+/**
+*function file to set up functionality for jobping theme
+*/
+
+function jobping_theme_setup(){//set up theme
+
+	//add support for logo
+	add_theme_support('custom-logo',array(
+		'height'    =>100,
+		'width'     =>100,
+		'flex-width'=>true,
+		'flex-height'=>true,
+		'header-text'=> array('site-title','site-description'),
+  	));
+	
+	add_theme_support('title-tag');//wordpress provides title
+	
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 1200, 9999 );
+	
+	/*
+	* add array for custom header and add theme support for
+	* custom header.
+	*/
+    $jobping_header_settings = array(
+		'default-image'         =>'',
+		'width'                 =>'1366',
+		'height'                =>'250',
+		'flex-width'            =>'true',
+		'flex-height'           =>'true',
+		'uploads'               =>'true',
+		'header-text'           =>'true',
+		'default-text-color'    => '',
+		'wp-head-callback'      => '',
+		'admin-head-callback'   => '',
+		'admin-preview-callback'=> '',
+    );
+	add_theme_support('custom-header',$jobping_header_settings);
+	
+	/*
+	* add support for custom backgrounds.
+	*/
+	//add custom background arguments
+    $defaults = array(
+		'default-color'          => '',
+		'default-image'          => '',
+		'wp-head-callback'       => '_custom_background_cb',
+		'admin-head-callback'    => '',
+		'admin-preview-callback' => ''
+	);
+	add_theme_support( 'custom-background', $defaults );
+	
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+}
+add_action('after_setup_theme','jobping_theme_setup');
+
 //enqueue style sheets
 function enqueue_styles_jobping(){
 	
@@ -14,62 +76,12 @@ function enqueue_styles_jobping(){
 //call the function for css
 add_action('wp_enqueue_scripts', 'enqueue_styles_jobping');
 
-//function for images
-function logo_insert_jobping(){
-	
-    add_theme_support('custom-logo',array(
-        'height'    =>100,
-        'width'     =>100,
-        'flex-width'=>true,
-        'flex-height'=>true,
-        'header-text'=> array('site-title','site-description'),
-    ));
-}
-//add action for logo
-add_action('after_setup_theme','logo_insert_jobping');
-
 //output logout
 function jobping_logo_output(){
     if (function_exists('the_custom_logo')){
         the_custom_logo();
     }
 }
-
-//add support for custom header.
-function jobping_header_custom_support(){
-    //add theme support for the custom header
-    $jobping_header_settings = array(
-    'default-image'         =>'',
-    'width'                 =>'1366',
-    'height'                =>'250',
-    'flex-width'            =>'true',
-    'flex-height'           =>'true',
-    'uploads'               =>'true',
-    'header-text'           =>'true',
-    'default-text-color'    => '',
-	'wp-head-callback'      => '',
-	'admin-head-callback'   => '',
-	'admin-preview-callback'=> '',
-    );
-    //add theme support
-    add_theme_support('custom-header',$jobping_header_settings);
-}
-//add the action to active it
-add_action('after_setup_theme','jobping_header_custom_support');
-
-//create function to allow custom backgrounds
-function jobping_custom_background(){
-    //add custom background arguments
-    $defaults = array(
-    'default-color'          => '',
-    'default-image'          => '',
-    'wp-head-callback'       => '_custom_background_cb',
-    'admin-head-callback'    => '',
-    'admin-preview-callback' => ''
-);
-add_theme_support( 'custom-background', $defaults );
-}
-add_action('after_setup_theme','jobping_custom_background');
 
 //register navigation menu
 function jobping_register_nav(){
