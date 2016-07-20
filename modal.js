cdbedfa80ad964e167b0a5242456556188fcd018
Grant Spooner-Roberts <grantroberts@live.co.uk>
2016-07-20 19:12:20 +0100

    Create modal.js
    Adds functionality to my html5/css3 modal box using simple jquery.

---
+++ modal.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".signIn").click(function(){
+        $(".signInPage").show();
+    });
+    
+    $(".signInClose").click(function(){
+       $(".signInPage").hide(); 
+    });
+    
+});
